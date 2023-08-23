524d7d478c0464b335dc7cf174cbd248d7c2f1dd
josueprogram2003 <73667027+josueprogram2003@users.noreply.github.com>
2023-08-22 20:13:18 -0500

    spo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const epxress = require('express')
 const cors = require('cors')
 const app = epxress()
-const { dbConnect } = require('./config/mongo')
+const { dbConnect } = require('./app/config/mongo')
 
 const PORT = process.env.PORT || 3000
 app.use(cors())
