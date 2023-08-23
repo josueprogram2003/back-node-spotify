8b6a751e9b949a104e33e2fc150b984fca965a5e
josueprogram2003 <73667027+josueprogram2003@users.noreply.github.com>
2023-08-22 22:52:40 -0500

    changes

--- app/app.js
+++ app/app.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 const epxress = require('express')
 const cors = require('cors')
 const app = epxress()
-const { dbConnect } = require('./app/config/mongo')
+const { dbConnect } = require('./config/mongo')
 
 const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(epxress.json())
 app.use(epxress.static('public'));
-app.use('/api/1.0', require('./app/routes'))
+app.use('/api/1.0', require('./routes'))
 
 app.listen(PORT, () => {
     console.log(`Tu API es http://localhost:${PORT}/api/1.0`)
